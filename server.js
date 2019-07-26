f5b9d9982ffb9a6f72f4d8e29493b45762915b9f
Animesh Paul <animeshpaul.connect@gmail.com>
2019-07-26 06:03:30 +0530

    adding path in server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
+const path = require('path')
 
 const posts = require('./routes/api/posts');
 const app = express()
@@ -15,6 +16,14 @@ mongoose
     .catch(error => console.log('db error',error));
 
 app.use('/api/posts', posts)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
