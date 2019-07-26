31da3e18e54d9c6dc952efa8e9c25342bd5884ec
Animesh Paul <animeshpaul.connect@gmail.com>
2019-07-26 07:50:05 +0530

    adding README.md file

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,12 @@
 module.exports = {
     mongoURI: 'mongodb+srv://paulanimesh:animesh123@cluster0-dxkxw.mongodb.net/test?retryWrites=true&w=majority'
 }
+
+// const MongoClient = require(‘mongodb’).MongoClient;
+// const uri = "mongodb+srv://paulanimesh:<password>@cluster0-dxkxw.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
